23187524a07137d76602156694d640c2838e1574
juan1432 <juanse1432@hotmail.es>
2021-07-24 23:42:14 -0500

    creando usuario

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #local APPs
 
     'post',
+    'users',
 ]
 
 MIDDLEWARE = [
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from post import views as post_views
+from  django.contrib import  admin
 urlpatterns = [
     path('hello-world/',local_views.hello_wolrd),
     path('sorted/',local_views.sort_numbers),
     path('hi/<str:name>/<int:age>',local_views.hi),
-    path('post/', post_views.list_post )
+    path('post/', post_views.list_post ),
+    path('admin', admin.site.urls)
 ]
